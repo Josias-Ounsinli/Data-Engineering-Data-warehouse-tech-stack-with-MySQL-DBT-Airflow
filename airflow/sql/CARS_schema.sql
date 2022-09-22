8d28efbde5d5e81111934be7fcf80a77443c5d31
Josias-Ounsinli <josias.ouns.djossou@gmail.com>
2022-09-22 10:12:08 +0100

    sql schema

--- airflow/sql/CARS_schema.sql
+++ airflow/sql/CARS_schema.sql
@@ -1,13 +1,8 @@
-CREATE TABLE IF NOT EXISTS vehicles
+CREATE TABLE IF NOT EXISTS CARS
 (
-    "id" INT NOT NULL AUTO_INCREMENT,
     "track_id" TEXT NOT NULL,
-    "lat" TEXT NOT NULL,
-    "lon" TEXT DEFAULT NULL,
-    "speed" TEXT DEFAULT NULL,
-    "lon_acc" TEXT DEFAULT NULL,
-    "lat_acc" TEXT DEFAULT NULL,
-    "time" TEXT DEFAULT NULL,
-    PRIMARY KEY ("id"),
-    FOREIGN KEY ('track_id') REFERENCES CARS('track_id')
+    "type" TEXT NOT NULL,
+    "traveled_d" TEXT DEFAULT NULL,
+    "avg_speed" TEXT DEFAULT NULL,
+    PRIMARY KEY ("track_id")
 );
